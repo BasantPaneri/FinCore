8e25e32686c4f798621d9aa4e4685138f1ac5e78
Basant <paneribasant987@gmail.com>
2026-06-11 12:49:01 +0530

    FIX

--- UPI/src/main/java/com/FinCore/UPI/UpiApplication.java
+++ UPI/src/main/java/com/FinCore/UPI/UpiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UpiApplication {
+public class UpiApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UpiApplication.class, args);
